befd6b2b6004ac7f51847813514ea57842bff2a2
Anirudh Kamath <github@kamath.io>
2025-11-27 16:32:42 -1000

    add editor for user solution

--- apps/web/app/problem/[problemId]/components/problem-render.tsx
+++ apps/web/app/problem/[problemId]/components/problem-render.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+import Editor from "@monaco-editor/react";
 import { Button } from "@/components/ui/button";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { useEffect, useState } from "react";
 import { MessageResponse } from "@/components/ai-elements/message";
 import Loader from "@/components/client/loader";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 import {
   useProblemText,
   useTestCases,
@@ -80,251 +86,276 @@ export default function ProblemRender({ problemId }: { problemId: string }) {
   }, [solution]);
 
   return (
-    <div>
-      <div>Problem: {problemId}</div>
-      <div>
-        {!problemText && (
-          <>
-            <Button
-              variant={"outline"}
-              onClick={() => callGenerateProblemText()}
-            >
-              Generate Problem Text
-            </Button>
-            <Button variant={"outline"} onClick={() => getProblemText()}>
-              Get Problem Text
-            </Button>
-          </>
-        )}
-        {isProblemTextLoading ? (
-          <Loader />
-        ) : (
-          <>
-            {problemTextError && (
-              <Alert variant="destructive" className="mb-4">
-                <AlertTitle>Error</AlertTitle>
-                <AlertDescription>
-                  {problemTextError instanceof Error
-                    ? problemTextError.message
-                    : String(problemTextError)}
-                </AlertDescription>
-              </Alert>
-            )}
-            {problemText && (
+    <div className="h-screen w-screen">
+      <ResizablePanelGroup direction="horizontal" className="h-full w-full">
+        <ResizablePanel defaultSize={20} className="h-full">
+          <div className="h-full overflow-auto">
+            <div>Problem: {problemId}</div>
+            <div>
+              {!problemText && (
+                <>
+                  <Button
+                    variant={"outline"}
+                    onClick={() => callGenerateProblemText()}
+                  >
+                    Generate Problem Text
+                  </Button>
+                  <Button variant={"outline"} onClick={() => getProblemText()}>
+                    Get Problem Text
+                  </Button>
+                </>
+              )}
+              {isProblemTextLoading ? (
+                <Loader />
+              ) : (
+                <>
+                  {problemTextError && (
+                    <Alert variant="destructive" className="mb-4">
+                      <AlertTitle>Error</AlertTitle>
+                      <AlertDescription>
+                        {problemTextError instanceof Error
+                          ? problemTextError.message
+                          : String(problemTextError)}
+                      </AlertDescription>
+                    </Alert>
+                  )}
+                  {problemText && (
+                    <>
+                      <MessageResponse>
+                        {problemText.problemText}
+                      </MessageResponse>
+                      <MessageResponse>
+                        {problemText.functionSignature}
+                      </MessageResponse>
+                    </>
+                  )}
+                </>
+              )}
+            </div>
+            <div>
+              <Button
+                variant={"outline"}
+                onClick={() => callGenerateTestCases()}
+              >
+                Generate Test Case Descriptions
+              </Button>
+              <Button variant={"outline"} onClick={() => getTestCases()}>
+                Get Test Case Descriptions
+              </Button>
+              {isTestCasesLoading ? (
+                <Loader />
+              ) : (
+                <>
+                  {testCasesError && (
+                    <Alert variant="destructive" className="mb-4">
+                      <AlertTitle>Error</AlertTitle>
+                      <AlertDescription>
+                        {testCasesError instanceof Error
+                          ? testCasesError.message
+                          : String(testCasesError)}
+                      </AlertDescription>
+                    </Alert>
+                  )}
+                  {testCases && (
+                    <div>
+                      {testCases.map((testCase, i) => (
+                        <div key={`testcase-description-${i}`}>
+                          {testCase.description}
+                          {testCase.isEdgeCase ? " [Edge Case]" : ""}
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </>
+              )}
+            </div>
+            <div>
+              <Button
+                variant={"outline"}
+                onClick={() => callGenerateTestCaseInputCode()}
+              >
+                Generate Test Case Inputs
+              </Button>
+              <Button
+                variant={"outline"}
+                onClick={() => getCodeToGenerateTestCaseInputs()}
+              >
+                Get Test Case Inputs
+              </Button>
+              {isTestCaseInputsLoading ? (
+                <Loader />
+              ) : (
+                <>
+                  {testCaseInputCodeError && (
+                    <Alert variant="destructive" className="mb-4">
+                      <AlertTitle>Error</AlertTitle>
+                      <AlertDescription>
+                        {testCaseInputCodeError instanceof Error
+                          ? testCaseInputCodeError.message
+                          : String(testCaseInputCodeError)}
+                      </AlertDescription>
+                    </Alert>
+                  )}
+                  {testCaseInputCode && (
+                    <div>
+                      {testCaseInputCode.map((testCaseInput, i) => (
+                        <div key={`testcase-input-${i}`}>{testCaseInput}</div>
+                      ))}
+                    </div>
+                  )}
+                </>
+              )}
+            </div>
+            <div>
+              <Button
+                variant={"outline"}
+                onClick={() => callGenerateTestCaseInputs()}
+              >
+                Run Generate Input
+              </Button>
+              <Button variant={"outline"} onClick={() => getTestCaseInputs()}>
+                Get Test Case Inputs
+              </Button>
+              {isGenerateTestCaseInputsLoading ? (
+                <Loader />
+              ) : (
+                <>
+                  {testCaseInputsError && (
+                    <Alert variant="destructive" className="mb-4">
+                      <AlertTitle>Error</AlertTitle>
+                      <AlertDescription>
+                        {testCaseInputsError instanceof Error
+                          ? testCaseInputsError.message
+                          : String(testCaseInputsError)}
+                      </AlertDescription>
+                    </Alert>
+                  )}
+                  {testCaseInputs && (
+                    <div>
+                      {testCaseInputs.map((result, i) => (
+                        <div key={`run-generate-input-result-${i}`}>
+                          {JSON.stringify(result)}
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </>
+              )}
+            </div>
+            <div>
+              <Button
+                variant={"outline"}
+                onClick={() => callGenerateSolution()}
+              >
+                Generate Solution
+              </Button>
+              <Button variant={"outline"} onClick={() => getSolution()}>
+                Get Solution
+              </Button>
+              {isGenerateSolutionLoading ? (
+                <Loader />
+              ) : (
+                <>
+                  {solutionError && (
+                    <Alert variant="destructive" className="mb-4">
+                      <AlertTitle>Error</AlertTitle>
+                      <AlertDescription>
+                        {solutionError instanceof Error
+                          ? solutionError.message
+                          : String(solutionError)}
+                      </AlertDescription>
+                    </Alert>
+                  )}
+                  {solution && <MessageResponse>{solution}</MessageResponse>}
+                </>
+              )}
+            </div>
+            <div>
+              <Button
+                variant={"outline"}
+                onClick={() => callGenerateTestCaseOutputs()}
+              >
+                Generate Test Case Outputs
+              </Button>
+              <Button variant={"outline"} onClick={() => getTestCaseOutputs()}>
+                Get Test Case Outputs
+              </Button>
+              {isGenerateTestCaseOutputsLoading ? (
+                <Loader />
+              ) : (
+                <>
+                  {testCaseOutputsError && (
+                    <Alert variant="destructive" className="mb-4">
+                      <AlertTitle>Error</AlertTitle>
+                      <AlertDescription>
+                        {testCaseOutputsError instanceof Error
+                          ? testCaseOutputsError.message
+                          : String(testCaseOutputsError)}
+                      </AlertDescription>
+                    </Alert>
+                  )}
+                  {testCaseOutputs && (
+                    <div>
+                      {testCaseOutputs.map((output, i) => (
+                        <div key={`testcase-output-${i}`}>
+                          {JSON.stringify(output)}
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </>
+              )}
+            </div>
+            <div>
+              <Button variant={"outline"} onClick={() => callRunUserSolution()}>
+                Run User Solution
+              </Button>
+            </div>
+            {isRunUserSolutionLoading ? (
+              <Loader />
+            ) : (
               <>
-                <MessageResponse>{problemText.problemText}</MessageResponse>
-                <MessageResponse>
-                  {problemText.functionSignature}
-                </MessageResponse>
+                {userSolutionError && (
+                  <Alert variant="destructive" className="mb-4">
+                    <AlertTitle>Error</AlertTitle>
+                    <AlertDescription>
+                      {userSolutionError instanceof Error
+                        ? userSolutionError.message
+                        : String(userSolutionError)}
+                    </AlertDescription>
+                  </Alert>
+                )}
+                {userSolutionTestResults && (
+                  <div>
+                    {userSolutionTestResults.map((testResult, i) => (
+                      <div key={`user-solution-test-result-${i}`}>
+                        {JSON.stringify({
+                          testCase: testResult.testCase.description,
+                          status: testResult.status,
+                          actual: testResult.actual,
+                          error: testResult.error,
+                          expected: testResult.testCase.expected,
+                        })}
+                      </div>
+                    ))}
+                  </div>
+                )}
               </>
             )}
-          </>
-        )}
-      </div>
-      <div>
-        <Button variant={"outline"} onClick={() => callGenerateTestCases()}>
-          Generate Test Case Descriptions
-        </Button>
-        <Button variant={"outline"} onClick={() => getTestCases()}>
-          Get Test Case Descriptions
-        </Button>
-        {isTestCasesLoading ? (
-          <Loader />
-        ) : (
-          <>
-            {testCasesError && (
-              <Alert variant="destructive" className="mb-4">
-                <AlertTitle>Error</AlertTitle>
-                <AlertDescription>
-                  {testCasesError instanceof Error
-                    ? testCasesError.message
-                    : String(testCasesError)}
-                </AlertDescription>
-              </Alert>
-            )}
-            {testCases && (
-              <div>
-                {testCases.map((testCase, i) => (
-                  <div key={`testcase-description-${i}`}>
-                    {testCase.description}
-                    {testCase.isEdgeCase ? " [Edge Case]" : ""}
-                  </div>
-                ))}
-              </div>
-            )}
-          </>
-        )}
-      </div>
-      <div>
-        <Button
-          variant={"outline"}
-          onClick={() => callGenerateTestCaseInputCode()}
-        >
-          Generate Test Case Inputs
-        </Button>
-        <Button
-          variant={"outline"}
-          onClick={() => getCodeToGenerateTestCaseInputs()}
-        >
-          Get Test Case Inputs
-        </Button>
-        {isTestCaseInputsLoading ? (
-          <Loader />
-        ) : (
-          <>
-            {testCaseInputCodeError && (
-              <Alert variant="destructive" className="mb-4">
-                <AlertTitle>Error</AlertTitle>
-                <AlertDescription>
-                  {testCaseInputCodeError instanceof Error
-                    ? testCaseInputCodeError.message
-                    : String(testCaseInputCodeError)}
-                </AlertDescription>
-              </Alert>
-            )}
-            {testCaseInputCode && (
-              <div>
-                {testCaseInputCode.map((testCaseInput, i) => (
-                  <div key={`testcase-input-${i}`}>{testCaseInput}</div>
-                ))}
-              </div>
-            )}
-          </>
-        )}
-      </div>
-      <div>
-        <Button
-          variant={"outline"}
-          onClick={() => callGenerateTestCaseInputs()}
-        >
-          Run Generate Input
-        </Button>
-        <Button variant={"outline"} onClick={() => getTestCaseInputs()}>
-          Get Test Case Inputs
-        </Button>
-        {isGenerateTestCaseInputsLoading ? (
-          <Loader />
-        ) : (
-          <>
-            {testCaseInputsError && (
-              <Alert variant="destructive" className="mb-4">
-                <AlertTitle>Error</AlertTitle>
-                <AlertDescription>
-                  {testCaseInputsError instanceof Error
-                    ? testCaseInputsError.message
-                    : String(testCaseInputsError)}
-                </AlertDescription>
-              </Alert>
-            )}
-            {testCaseInputs && (
-              <div>
-                {testCaseInputs.map((result, i) => (
-                  <div key={`run-generate-input-result-${i}`}>
-                    {JSON.stringify(result)}
-                  </div>
-                ))}
-              </div>
-            )}
-          </>
-        )}
-      </div>
-      <div>
-        <Button variant={"outline"} onClick={() => callGenerateSolution()}>
-          Generate Solution
-        </Button>
-        <Button variant={"outline"} onClick={() => getSolution()}>
-          Get Solution
-        </Button>
-        {isGenerateSolutionLoading ? (
-          <Loader />
-        ) : (
-          <>
-            {solutionError && (
-              <Alert variant="destructive" className="mb-4">
-                <AlertTitle>Error</AlertTitle>
-                <AlertDescription>
-                  {solutionError instanceof Error
-                    ? solutionError.message
-                    : String(solutionError)}
-                </AlertDescription>
-              </Alert>
-            )}
-            {solution && <MessageResponse>{solution}</MessageResponse>}
-          </>
-        )}
-      </div>
-      <div>
-        <Button
-          variant={"outline"}
-          onClick={() => callGenerateTestCaseOutputs()}
-        >
-          Generate Test Case Outputs
-        </Button>
-        <Button variant={"outline"} onClick={() => getTestCaseOutputs()}>
-          Get Test Case Outputs
-        </Button>
-        {isGenerateTestCaseOutputsLoading ? (
-          <Loader />
-        ) : (
-          <>
-            {testCaseOutputsError && (
-              <Alert variant="destructive" className="mb-4">
-                <AlertTitle>Error</AlertTitle>
-                <AlertDescription>
-                  {testCaseOutputsError instanceof Error
-                    ? testCaseOutputsError.message
-                    : String(testCaseOutputsError)}
-                </AlertDescription>
-              </Alert>
-            )}
-            {testCaseOutputs && (
-              <div>
-                {testCaseOutputs.map((output, i) => (
-                  <div key={`testcase-output-${i}`}>
-                    {JSON.stringify(output)}
-                  </div>
-                ))}
-              </div>
-            )}
-          </>
-        )}
-      </div>
-      <div>
-        <Button variant={"outline"} onClick={() => callRunUserSolution()}>
-          Run User Solution
-        </Button>
-      </div>
-      {isRunUserSolutionLoading ? (
-        <Loader />
-      ) : (
-        <>
-          {userSolutionError && (
-            <Alert variant="destructive" className="mb-4">
-              <AlertTitle>Error</AlertTitle>
-              <AlertDescription>
-                {userSolutionError instanceof Error
-                  ? userSolutionError.message
-                  : String(userSolutionError)}
-              </AlertDescription>
-            </Alert>
-          )}
-          {userSolutionTestResults && (
-            <div>
-              {userSolutionTestResults.map((testResult, i) => (
-                <div key={`user-solution-test-result-${i}`}>
-                  {JSON.stringify({
-                    testCase: testResult.testCase.description,
-                    status: testResult.status,
-                    actual: testResult.actual,
-                    error: testResult.error,
-                    expected: testResult.testCase.expected,
-                  })}
-                </div>
-              ))}
-            </div>
-          )}
-        </>
-      )}
+          </div>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={50} className="bg-blue-100">
+          <Editor
+            height="100%"
+            width="100%"
+            //   theme="vs-dark"
+            defaultLanguage="typescript"
+            value={userSolution ?? ""}
+            onChange={(value) => setUserSolution(value ?? null)}
+          />
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 }
